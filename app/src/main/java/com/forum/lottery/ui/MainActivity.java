c03ebe0650d7b431a64e1bf3945075df85526469
liu0000006 <liu0000006@gmail.com>
2018-01-18 01:55:27 +0800

    modify1

--- app/src/main/java/com/forum/lottery/ui/MainActivity.java
+++ app/src/main/java/com/forum/lottery/ui/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends BaseActivity {
 
         long t1 = System.currentTimeMillis();
 
-        LogUtils.i("time1--t1-t1->" + t1);
+        LogUtils.i("time1--t1-t1->t1" + t1);
     }
 }
